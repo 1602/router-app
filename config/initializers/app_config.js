f15a3afe4c9770dad0b5a2ec8466d0ecd4156cb5
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.ru>
2011-03-01 10:14:15 +0300

    Updated initializer

--- config/initializers/app_config.js
+++ config/initializers/app_config.js
@@ -1,6 +1,6 @@
 // Load config
 global.config = app.config = require('yaml').eval(
     require('fs')
-    .readFileSync(root_dir + '/config/app_config.yml')
+    .readFileSync(app.root + '/config/app_config.yml')
     .toString()
 )[app && app.settings && app.settings.env || 'development'];
